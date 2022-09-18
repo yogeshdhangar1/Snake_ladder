a9167dd5d5a56ea2b7dc47c92f1a4ff8a328fe06
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-09-18 02:19:06 -0700

    Added: Game At Start Position

--- src/main/java/Snake_ladder.java
+++ src/main/java/Snake_ladder.java
@@ -1,5 +1,7 @@
 public class Snake_ladder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
+        int position = 0;
+        System.out.println("Start Position At :"+ position);
     }
 }
