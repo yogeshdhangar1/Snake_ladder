268d5961400b66546d3b4b993fe981d1f8d8eec2
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-09-18 05:38:59 -0700

    Added: player rechaes wining position

--- src/main/java/Playsnakeladder.java
+++ src/main/java/Playsnakeladder.java
@@ -1,31 +1,36 @@
 import java.util.Random;
 public class Playsnakeladder {
-   public static final int noplay = 1;
+    public static final int noplay = 1;
     public static final int snake = 2;
-     public static final int ladder = 3;
+    public static final int ladder = 3;
 
     public static void main(String[] args) {
         int start = 0;
-        while(start<=100){
-            if(start==100){
+        while (start <= 100) {
+            if (start == 100) {
+                System.out.println("You Won");
                 break;
             }
-            int dice = (int)Math.floor(Math.random()*10%6+1);
-            int option = (int)Math.floor(Math.random()*10%3+1);
+            int dice = (int) Math.floor(Math.random() * 10 % 6 + 1);
+            int option = (int) Math.floor(Math.random() * 10 % 3 + 1);
 
             // case statement
-            switch(option){
-                case noplay:
-                    start = start=0;
-                    break;
-                case ladder:
+            switch (option) {
+                case 1:
+                     if(start+dice<=100){
                     start = start+dice;
+                }
+                      break;
+                case 2:
+                    if(start-dice>=0){
+                        start = start-dice;
+                    }
                     break;
-                case snake:
-                    start = start-dice;
-                    break;
+                case 3:
+                    start= start+0;
             }
-            System.out.println("Postion of player:" + start);
+            System.out.println("Position of The player :"+start);
         }
     }
 }
+
