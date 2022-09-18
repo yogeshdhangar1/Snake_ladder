3bf0753cf4ff2e44faf6fdc3ebaa5e9b47d5908a
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-09-18 02:27:53 -0700

    Added: Roll The Dice

--- src/main/java/Snake_ladder.java
+++
@@ -1,7 +0,0 @@
-public class Snake_ladder {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Snake And Ladder Game");
-        int position = 0;
-        System.out.println("Start Position At :"+ position);
-    }
-}
